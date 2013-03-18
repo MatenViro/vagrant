aaf8aa1a256f7cf0e82646b445b2b259c613048d
MatenViro <matenviro@gmail.com>
2013-03-18 16:30:05 +0400

    Share mysqld server to connections from host-machine through 3306 port-forwarding

--- vagrant/puppet/manifests/init.pp
+++ vagrant/puppet/manifests/init.pp
@@ -15,14 +15,15 @@ apache::vhost {'localhost':
 class {'mysql::php':}
 class {'mysql::server':
 	config_hash => {
-		'root_password' => 'root'
-    }
+		'root_password' => 'root',
+		'bind_address' => '0.0.0.0'
+	}
 }
 
 mysql::db {'vgdb':
 	user => 'vagrant',
 	password => 'vagrant',
-	host => 'localhost',
+	host => '%',
 	grant => ['all'],
 	charset => 'utf8',
 	ensure => present
